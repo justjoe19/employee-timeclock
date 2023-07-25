07ba3a4911ac3a0064f8d6edd79c3730dde9208b
gnetabai <netabaig@gmail.com>
2023-07-25 15:03:33 -0400

    Implementing Gheleb branch functions/models/db.

--- timeclock/models.py
+++ timeclock/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.postgres.fields import DateRangeField
 
 class Employee(models.Model):
     employee_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=100, default='')
+    employed = models.BooleanField(default=True)
+    pto = models.DecimalField(default=0, decimal_places=2, max_digits=5)
 
     def is_clocked_in(self):
         latest_punch = self.punches.order_by('-punch_time').first()
@@ -25,4 +28,13 @@ class Punch(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='punches')
     punch_time = models.DateTimeField()
     punch_type = models.CharField(max_length=20)
+
+class LOA(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='leave_requests')
+    request = DateRangeField()
+    time_submitted = models.DateTimeField()
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"LOA Request #{self.id} for {self.employee.name}"
 
